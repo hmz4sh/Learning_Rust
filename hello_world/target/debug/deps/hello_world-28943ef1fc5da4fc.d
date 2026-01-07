4dd0884ac0886b5ff50f6c6fe1df7ec022fed72e
Hamza Shahzad <i256520@isb.nu.edu.pk>
2026-01-07 09:31:07 +0500

    cargo stuff

---
+++ hello_world/target/debug/deps/hello_world-28943ef1fc5da4fc.d
@@ -0,0 +1,5 @@
+/home/hamza/coding/rust/hello_world/target/debug/deps/hello_world-28943ef1fc5da4fc.d: src/main.rs
+
+/home/hamza/coding/rust/hello_world/target/debug/deps/hello_world-28943ef1fc5da4fc: src/main.rs
+
+src/main.rs:
